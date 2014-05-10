70f18647a8b7c262d23214f2525431353568d5a0
nsonti <nikhilsv92@gmail.com>
2014-05-10 16:03:02 +0530

    [nikhil] added function to get address from coordinates

--- MapsRequestAPI.py
+++ MapsRequestAPI.py
@@ -23,9 +23,8 @@ def get_coor_from_address(address):
 
     # sample output
     # {'status': 'OK', 'results': [{'geometry': {'location': {'lat': 12.9327621, 'lng': 77.6140289}, 'viewport': {'northeast': {'lat': 12.9341110802915, 'lng': 77.6153778802915}, 'southwest': {'lat': 12.9314131197085, 'lng': 77.6126799197085}}, 'location_type': 'APPROXIMATE'}, 'address_components': [{'long_name': 'Akamai Technologies India Pvt Ltd', 'types': ['premise'], 'short_name': 'Akamai Technologies India Pvt Ltd'}, {'long_name': 'Jyoti Nivas College Road', 'types': ['route'], 'short_name': 'Jyoti Nivas College Road'}, {'long_name': 'Koramangala Industrial Layout', 'types': ['sublocality', 'political'], 'short_name': 'Koramangala Industrial Layout'}, {'long_name': 'Koramangala', 'types': ['sublocality', 'political'], 'short_name': 'Koramangala'}, {'long_name': 'Bangalore', 'types': ['locality', 'political'], 'short_name': 'Bangalore'}, {'long_name': 'Bangalore Urban', 'types': ['administrative_area_level_2', 'political'], 'short_name': 'Bangalore Urban'}, {'long_name': 'Karnataka', 'types': ['administrative_area_level_1', 'political'], 'short_name': 'KA'}, {'long_name': 'India', 'types': ['country', 'political'], 'short_name': 'IN'}, {'long_name': '560034', 'types': ['postal_code'], 'short_name': '560034'}], 'partial_match': True, 'formatted_address': 'Akamai Technologies India Pvt Ltd, Jyoti Nivas College Road, Koramangala Industrial Layout, Koramangala, Bangalore, Karnataka 560034, India', 'types': ['premise']}]}
-    lat = result['results'][0]['geometry'][0]['location']['lat']
-    lng = result['results'][0]['geometry'][0]['location']['lng']
-
+    lat = result['results'][0]['geometry']['location']['lat']
+    lng = result['results'][0]['geometry']['location']['lng']
     res_dict = {}
     res_dict['lat'] = lat
     res_dict['lng'] = lng
