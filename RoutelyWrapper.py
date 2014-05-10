2238628245f73bf7d0f877e89630561b1b0826c5
nsonti <nikhilsv92@gmail.com>
2014-05-10 22:11:51 +0530

    [nikhil] RoutelyWrapper.py: Added multiple center while calculating the cost

--- RoutelyWrapper.py
+++ RoutelyWrapper.py
@@ -1,11 +1,17 @@
 from MapsRequestAPI import  *
 from DBStuffAPI import *
 from XlsxReader import *
+from MapRequestThreadPool import MapRequestThreadPool
 
 center_1_latlng = {}
 center_1_latlng['lat'] = 13.001658
 center_1_latlng['lng'] = 77.551099
 
+center_2_latlng = {}
+center_2_latlng['lat'] = 12.9261416
+center_2_latlng['lng'] = 77.5975514
+
+center_latlng = [{'lat' : 13.001658, 'lng' : 77.551099}, {'lat' : 12.9261416, 'lng' : 77.5975514}]
 class XlxsToMapsWrapper:
     LAT_LOG_FILE_NAME = '../akshaya-patra-data/lon and log details-xls.xlsx'
     def __init__(self):
@@ -25,32 +31,42 @@ class GetAndStoreCost:
         pass
 
     def compute_costs(self):
+        #TODO:nikhil - Add the 2nd center path lenghts
         all_rows_list = self.lat_lng_db.get_all()
         #print all_rows_list[0:4]
         count = 0
         res_count_none = 0
         res_count_not_none  = 0
-        for row in all_rows_list:
-            try:
-                _id = row[u'_id']
-                if u'log' in row and u'lon' in row:
-                    lng = row[u'log']
-                    lat = row[u'lon']
-                    dest_coor = {}
-                    dest_coor['lat'] = float(lat)
-                    dest_coor['lng'] = float(lng)
-                    cost = self.maps_request_api.get_cost_from_coor(origin_coor=center_1_latlng, dest_coor=dest_coor)
-                    cost.update(dest_coor)
-                    cost['_id'] = _id
-                    self.lat_lng_cost_db.add_to_db(cost)
-            except:
-                print 'Got screwed!'
-            print 'res_count_none', res_count_none, 'res count not done', res_count_not_none
-            if count == 1:
-                break
-            count += 1
+        for center in center_latlng:
+            for row in all_rows_list:
+                    try:
+                        _id = row[u'_id']
+                        if u'log' in row and u'lon' in row:
+                            lng = row[u'log']
+                            lat = row[u'lon']
+                            dest_coor = {}
+                            dest_coor['lat'] = float(lat)
+                            dest_coor['lng'] = float(lng)
+                            cost = self.maps_request_api.get_cost_from_coor(origin_coor=center, dest_coor=dest_coor)
+                            cost['dest'] = dest_coor
+                            cost['origin'] = center
+                            cost['_id'] = _id
+                            self.lat_lng_cost_db.add_to_db(cost)
+                            print cost
+                    except:
+                        print 'Got screwed!'
+                    #print 'res_count_none', res_count_none, 'res count not done', res_count_not_none
+
+                    if count == 10:
+                        break
+                    count += 1
 
             print 'res_count_none', res_count_none, 'res count not done', res_count_not_none
+
+    def compute_costs_thread_pool(self):
+        all_rows_list = self.lat_lng_db.get_all()
+        map_request_thread_pool = MapRequestThreadPool(20)
+        return
 
 
 
