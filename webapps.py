87f68aaa572ff85eefc9e0caa48ff82cf289bd10
ahmedshabib <ahmed.bolwar@gmail.com>
2014-05-10 19:17:23 +0530

    Updated the DB to MongoDB webapp.py

--- webapps.py
+++ webapps.py
@@ -1,70 +1,38 @@
 import time
-from sqlite3 import dbapi2 as sqlite3
 from hashlib import md5
 from datetime import datetime
-from flask import Flask, request, session, url_for, redirect, \
-     render_template, abort, g, flash, _app_ctx_stack
-from werkzeug import check_password_hash, generate_password_hash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask.ext.pymongo import PyMongo
+import json
+import uuid
+from werkzeug.utils import secure_filename
+from werkzeug.security import check_password_hash, generate_password_hash
+import os
 
 
-# configuration
-DATABASE = '/tmp/minitwit.db'
-PER_PAGE = 30
 DEBUG = True
 SECRET_KEY = 'development key'
 
-# create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
+app.config.from_envvar('ROUTLEY', silent=True)
+mongo = PyMongo(app)
 
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        top.sqlite_db.row_factory = sqlite3.Row
-    return top.sqlite_db
+@app.before_request
+def before_request():
+    g.user = None
 
-
-@app.teardown_appcontext
-def close_database(exception):
-    """Closes the database again at the end of the request."""
-    top = _app_ctx_stack.top
-    if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
-
-
-def init_db():
-    """Initializes the database."""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
-
-
-def query_db(query, args=(), one=False):
-    """Queries the database and returns a list of dictionaries."""
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    return (rv[0] if rv else None) if one else rv
+    if 'user_id' in session:
+        rv = mongo.db.users.find({"user_id": session['user_id']})
+        g.user = rv[0] if rv.count() != 0 else None
 
 
 def get_user_id(username):
     """Convenience method to look up the id for a username."""
-    rv = query_db('select user_id from user where username = ?',
-                  [username], one=True)
-    return rv[0] if rv else None
+    rv = mongo.db.user.find({"username": username})
+    return rv[0]["user_id"] if rv.count() != 0 else None
+
 
 
 def format_datetime(timestamp):
@@ -77,13 +45,13 @@ def gravatar_url(email, size=80):
     return 'http://www.gravatar.com/avatar/%s?d=identicon&s=%d' % \
         (md5(email.strip().lower().encode('utf-8')).hexdigest(), size)
 
-
 @app.before_request
 def before_request():
     g.user = None
+
     if 'user_id' in session:
-        g.user = query_db('select * from user where user_id = ?',
-                          [session['user_id']], one=True)
+        rv = mongo.db.users.find({"user_id": session['user_id']})
+        g.user = rv[0] if rv.count() != 0 else None
 
 
 @app.route('/')
@@ -94,14 +62,7 @@ def index():
     """
     if not g.user:
         return render_template('index.html')
-    return render_template('dashboard.html', messages=query_db('''
-        select message.*, user.* from message, user
-        where message.author_id = user.user_id and (
-            user.user_id = ? or
-            user.user_id in (select whom_id from follower
-                                    where who_id = ?))
-        order by message.pub_date desc limit ?''',
-        [session['user_id'], session['user_id'], PER_PAGE]))
+    return render_template('dashboard.html',   [session['user_id'], session['user_id'], 30])
 
 
 
@@ -113,8 +74,8 @@ def login():
         return redirect(url_for('timeline'))
     error = None
     if request.method == 'POST':
-        user = query_db('''select * from user where
-            username = ?''', [request.form['username']], one=True)
+        rv = mongo.db.users.find({"username": request.form['username']})
+        user = rv[0] if rv.count() != 0 else None
         if user is None:
             error = 'Invalid username'
         elif not check_password_hash(user['pw_hash'],
@@ -146,12 +107,9 @@ def register():
         elif get_user_id(request.form['username']) is not None:
             error = 'The username is already taken'
         else:
-            db = get_db()
-            db.execute('''insert into user (
-              username, email, pw_hash) values (?, ?, ?)''',
-              [request.form['username'], request.form['email'],
-               generate_password_hash(request.form['password'])])
-            db.commit()
+            mongo.db.users.insert(
+                {"username": request.form['username'], "email": request.form['email'],
+                 "pw_hash": generate_password_hash(request.form['password'])})
             flash('You were successfully registered and can login now')
             return redirect(url_for('login'))
     return render_template('register.html', error=error)
