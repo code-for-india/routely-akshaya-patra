6439fa9385388430bf2e03f6b7fa92450de9f758
nsonti <nikhilsv92@gmail.com>
2014-05-10 17:47:57 +0530

    [nikhil] Updated DBStuffAPI to include classes. Basic reading and writing from Xlsx file done in RoutelyWrapper.py

--- DBStuffAPI.py
+++ DBStuffAPI.py
@@ -1,11 +1,31 @@
 from pymongo import MongoClient
 
-db = MongoClient().routely
+class LngLatDB:
+    def __init__(self):
+        self.db = MongoClient().routely
 
-def add_to_db(start_point , end_point , distance,  time):
-    db.things.insert({"start_point": start_point,"end_point": end_point,"distance": distance,"time": time })
+    def add_to_db(self, row):
+        self.db.lnglat.insert(row)
 
+    def get_data(self, query_dict):
+        return list(self.db.lnglat.find(query_dict))
 
-def get_data(start_point, end_point):
-    return list(db.things.find({"start_point": start_point,"end_point": end_point}))
+class LngLatCostDB:
+    def __init__(self):
+        self.db = MongoClient.routely
+        pass
+
+    def add_to_db(self, row):
+        self.db.lnglatcost.insert(row)
+
+    def get_data(self, query_dict):
+        return list(self.db.lnglatcost.find(query_dict))
+
+# db = MongoClient().routely
+# def add_to_db(start_point , end_point , distance,  time):
+#     db.things.insert({"start_point": start_point,"end_point": end_point,"distance": distance,"time": time })
+#
+#
+# def get_data(start_point, end_point):
+#     return list(db.things.find({"start_point": start_point,"end_point": end_point}))
 
--- RoutelyWrapper.py
+++ RoutelyWrapper.py
@@ -1,8 +1,22 @@
 from MapsRequestAPI import  *
 from DBStuffAPI import *
+from XlsxReader import *
+
+
+
+class XlxsToMapsWrapper:
+    LAT_LOG_FILE_NAME = '../akshaya-patra-data/lon and log details-xls.xlsx'
+    def __init__(self):
+        self.lat_lng_db = LngLatDB()
+        pass
+
+    def getLatLngData(self, file_name=LAT_LOG_FILE_NAME):
+        self.rows = readXlsx(file_name, sheet=1, header=True)
+        self.lat_lng_db.add_to_db(self.rows)
 
 def main():
-
+    xlxs_to_maps = XlxsToMapsWrapper()
+    xlxs_to_maps.getLatLngData()
     return
 
 if __name__ == '__main__':
