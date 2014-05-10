06da905ed0634e49f157dbd9ffdf0f6d36506bc4
ahmedshabib <ahmed.bolwar@gmail.com>
2014-05-10 15:42:42 +0530

    I am kidding

--- dbstuff.py
+++ dbstuff.py
@@ -1,2 +1,12 @@
-import pymongo
+from pymongo import MongoClient
+
+db = MongoClient().routely
+
+def add_to_db(start_point , end_point , distance,  time):
+    db.things.insert({"start_point": start_point,"end_point": end_point,"distance": distance,"time": time })
+
+
+def get_data(start_point, end_point):
+    db.things.find({"start_point": start_point,"end_point": end_point})
+
 
